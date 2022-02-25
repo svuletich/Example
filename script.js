7897b4c43a2d65d340e6cb04ed348250f0e87d07
Patrick Farrell <56621096+contentnausea@users.noreply.github.com>
2022-02-25 15:45:01 -0700

    Add files via upload
    Some javascript

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Hello, world.");
+//Single line comment
+
+/*
+Multi-line comment
+12345
+6789
+*/
+
+(function() {
+  var newLink = document.createElement("a");
+  var allParagraphs = document.getElementsByTagName("p");
+  var firstParagraph = allParagraphs[0];
+
+  function toggleCopy(e) {
+    var allParagraphs = document.getElementsByTagName("p");
+
+    for (var i = 0; i < allParagraphs.length; i++) {
+      var para = allParagraphs[i];
+
+      if (i === 0) {
+        continue;
+      }
+
+      if (para.style.display === "none") {
+        para.style.display = "block";
+      } else {
+        para.style.display = "none";
+      }
+    }
+
+    if (this === newLink) {
+      this.remove();
+    }
+
+    if (e !== undefined && e.preventDefault !== undefined) {
+      e.preventDefault();
+    }
+  }
+
+  newLink.setAttribute("href", "#");
+  newLink.setAttribute("class", "more-link");
+  newLink.innerHTML = "Read more";
+
+  newLink.addEventListener("click", toggleCopy);
+
+  toggleCopy();
+
+  firstParagraph.appendChild(newLink);
+}());
